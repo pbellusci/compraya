0e18a40798d49196ac0e7942ed3c7b3e59cf44f0
Patricio Bellusci <patricio.bellusci@gmail.com>
2019-09-26 02:06:20 -0300

    Added git ignore

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 
+import SignUp from './components/SignUp.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/signup', component: SignUp }
+]
+
+const router = new VueRouter({routes: routes})
+
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
